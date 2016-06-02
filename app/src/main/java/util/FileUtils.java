5e8680c3e8d6c9252bff2a127552de6de0f98393
sunxipeng <1141351242@qq.com>
2016-06-02 18:09:19 +0800

    Initial commit

---
+++ app/src/main/java/util/FileUtils.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.File;
+
+
+public class FileUtils {
+
+
+    public static void deleteFile(File file) {
+        if(file == null || !file.exists()) {
+            return;
+        }
+        if(file.isFile()) {
+            final File to = new File( file.getAbsolutePath() + System.currentTimeMillis());
+            file.renameTo( to);
+            to.delete();
+        }
+        else {
+            File[] files = file.listFiles();
+            if(files != null && files.length > 0) {
+                for(File innerFile: files) {
+                    deleteFile( innerFile);
+                }
+            }
+            final File to = new File( file.getAbsolutePath() + System.currentTimeMillis());
+            file.renameTo( to);
+            to.delete();
+        }
+    }
+}
